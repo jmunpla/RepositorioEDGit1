2597ac4850080e6652c0cf4ebf499dc9b1c0b04f
juan <juan@LAPTOP-C90QPPCM>
2023-03-15 19:40:51 +0100

    Con esta versión creamos la primera rama a fecha de 15 de marzo de 2023

---
+++ src/paquete1/Principal.java
@@ -0,0 +1,10 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main (String [] args) {
+		System.out.println ("Estamos realizando un control de versiones con la herramienta Git");
+		
+		System.out.println ("Este mensaje es para continuar con la practica y modificar el programa");
+	}
+}
